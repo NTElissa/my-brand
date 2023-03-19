2cf5cf272c0c7f6e2e45cc429e371f3cfe0efac7
NTElissa <ntihindukaelissa77@gmail.com>
2023-03-19 12:55:28 +0200

    new update

--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,28 @@
-async function userLogin() {
-    const emailValue = document.getElementById("email").value;
-    const passwordValue = document.getElementById("password").value;
-    const data = { email: emailValue, password: passwordValue };
+
+
+async function userLogin(){
+    const emailValue = document.getElementById("email").value
+    const passwordValue = document.getElementById("password").value
+    const data = {email: emailValue, password: passwordValue}
     // use fetch method to interact with your login api endpoint
-  
-    await fetch("https://nice-red-chipmunk-tam.cyclic.app/api/v1/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+
+    await fetch('https://nice-red-chipmunk-tam.cyclic.app/api/v1/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.message === "Logged in successfully") {
-          alert(data.message);
-          window.location.href = "admin_home.html";
-        } else {
-          alert(data.message);
-        }
-      })
-      .catch((err) => alert(err));
-  }
-  
+        .then ((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            if (data.message==="Logged in successfully") {
+            
+                alert(data.message)
+                window.location.replace('admin_home.html');
+            } else {
+                alert(data.message)
+            }
+        })
+        .catch(err => alert(err))
+};
