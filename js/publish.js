2aab4f0d251d750bb692b0f237f7db7e69498e1a
NTElissa <ntihindukaelissa77@gmail.com>
2023-03-20 12:58:02 +0200

    new update

--- js/publish.js
+++ js/publish.js
@@ -10,10 +10,10 @@ function blogsRetrieve(){
         for (let i = post.length - 1; i >= post.length - 3; i--){
           let posts = `
           <div class="publish_blog">
-
-          <img src="${post[i].image}">
-          <p class="title_blog1">
-          ${post[i].body}
+           <p class="title_blog1">${post[i].title}
+          <img src="${post[i].image}"><b>
+        
+        <p>  ${post[i].body}
           </p>
     </div> ` 
     container.innerHTML += posts ;
